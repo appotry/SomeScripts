7213d5d3b41702a4995e2bcfcdbe3dc30943aacb
ooo <ooo@iMac.local>
2021-03-31 19:43:13 +0800

    update jd

--- jd_scripts/remote_crontab_list.sh
+++ jd_scripts/remote_crontab_list.sh
@@ -2,8 +2,6 @@
 0 10,11 * * 2-5 node /scripts/z_entertainment.js >> /scripts/logs/z_entertainment.log 2>&1
 # 粉丝互动
 15 10 * * * node /scripts/z_fanslove.js >> /scripts/logs/z_fanslove.log 2>&1
-# 联想集卡活动
-8 15 15-29 3 * node /scripts/z_lenovo.js >> /scripts/logs/z_lenovo.log 2>&1
 # 京东超市-大转盘
 10 10 * * * node /scripts/z_marketLottery.js >> /scripts/logs/z_marketLottery.log 2>&1
 # 母婴-跳一跳
@@ -12,8 +10,6 @@
 1 */8 * * * node /scripts/z_shake.js >> /scripts/logs/z_shake.log 2>&1
 # 5G超级盲盒
 1 1,6,11,16,21 * 3-4 * node /scripts/z_super5g.js >> /scripts/logs/z_super5g.log 2>&1
-# 京东小魔方
-5 10 25-27 3 * node /scripts/z_xmf.js >> /scripts/logs/z_xmf.log 2>&1
 # interCenter渠道店铺签到
 0 0 * * * node /scripts/monk_inter_shop_sign.js >> /scripts/logs/monk_inter_shop_sign.log 2>&1
 # 关注有礼
