c8d1c1311dc538391d2cb6a40c6ce0b382d93e9e
ooo <ooo@iMac.local>
2021-04-06 19:31:22 +0800

    update jd

--- jd_scripts/remote_crontab_list.sh
+++ jd_scripts/remote_crontab_list.sh
@@ -1,22 +1,26 @@
+# 加购有礼
+15 12 * * *  node /scripts/monk_shop_add_to_car.js >> /scripts/logs/monk_shop_add_to_car.log 2>&1
+# 创维408下班全勤奖
+15 8 2-18 4 * node /scripts/monk_skyworth_car.js >> /scripts/logs/monk_skyworth_car.log 2>&1
 # 百变大咖秀
 0 10,11 * * 2-5 node /scripts/z_entertainment.js >> /scripts/logs/z_entertainment.log 2>&1
 # 粉丝互动
-15 10 * * * node /scripts/z_fanslove.js >> /scripts/logs/z_fanslove.log 2>&1
+30 9 * * * node /scripts/z_fanslove.js >> /scripts/logs/z_fanslove.log 2>&1
 # 京东超市-大转盘
 10 10 * * * node /scripts/z_marketLottery.js >> /scripts/logs/z_marketLottery.log 2>&1
+# 母婴-跳一跳
+5 8,14,20 6-13 4 * node /scripts/z_mother_jump.js >> /scripts/logs/z_mother_jump.log 2>&1
 # 超级摇一摇
-3 */8 * * * node /scripts/z_shake.js >> /scripts/logs/z_shake.log 2>&1
+3 0,20 * * * node /scripts/z_shake.js >> /scripts/logs/z_shake.log 2>&1
 # 5G超级盲盒
-10 1,6,11,16,21 * 3-4 * node /scripts/z_super5g.js >> /scripts/logs/z_super5g.log 2>&1
+0 1,6,11,16,21 * 3-4 * node /scripts/z_super5g.js >> /scripts/logs/z_super5g.log 2>&1
 # 京东小魔方
 15 10 4-6 4 * node /scripts/z_xmf.js >> /scripts/logs/z_xmf.log 2>&1
+# 有机牧场
+0 0,1-22/2 1-31 4-7 * node /scripts/monk_pasture.js >> /scripts/logs/monk_pasture.log 2>&1
 # interCenter渠道店铺签到
 0 0 * * * node /scripts/monk_inter_shop_sign.js >> /scripts/logs/monk_inter_shop_sign.log 2>&1
 # 关注有礼
-10 15 * * * node /scripts/monk_shop_follow_sku.js >> /scripts/logs/monk_shop_follow_sku.log 2>&1
+15 15 * * * node /scripts/monk_shop_follow_sku.js >> /scripts/logs/monk_shop_follow_sku.log 2>&1
 # 店铺大转盘
-1 0,10,23 * * * node /scripts/monk_shop_lottery.js >> /scripts/logs/monk_shop_lottery.log 2>&1
-# 加购有礼
-15 12 * * *  node /scripts/monk_shop_add_to_car.js >> /scripts/logs/monk_shop_add_to_car.log 2>&1
-# 有机牧场
-0 0,1-22/2 1-31 4-7 * node /scripts/monk_pasture.js >> /scripts/logs/monk_pasture.log 2>&1
+3 0,10,23 * * * node /scripts/monk_shop_lottery.js >> /scripts/logs/monk_shop_lottery.log 2>&1
