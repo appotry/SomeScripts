77eca6fc537f312f37c47218eba193e263402d33
FKPYW <FKPYW>
2021-04-10 02:20:45 +0800

    update jd

--- jd_scripts/docker_entrypoint.sh
+++ jd_scripts/docker_entrypoint.sh
@@ -39,34 +39,19 @@ fi
 cd /jds
 git pull origin master --rebase
 
-#默认启动telegram交互机器人的条件
-#确认容器启动时调用的docker_entrypoint.sh
-#必须配置TG_BOT_TOKEN、TG_USER_ID，
-#且未配置DISABLE_BOT_COMMAND禁用交互，
-#且未配置自定义TG_API_HOST，因为配置了该变量，说明该容器环境可能并不能科学的连到telegram服务器
-if [[ -n "$run_cmd" && -n "$TG_BOT_TOKEN" && -n "$TG_USER_ID" && -z "$DISABLE_BOT_COMMAND" && -z "$TG_API_HOST" ]]; then
-    ENABLE_BOT_COMMAND=True
-else
-    ENABLE_BOT_COMMAND=False
-fi
-
 echo "------------------------------------------------执行定时任务任务shell脚本------------------------------------------------"
-sh /jds/jd_scripts/shell_default_script.sh "$ENABLE_BOT_COMMAND" "$run_cmd"
+sh /jds/jd_scripts/shell_default_script.sh "$run_cmd"
 echo "--------------------------------------------------默认定时任务执行完成---------------------------------------------------"
 
 if [ "$run_cmd" ]; then
-  if [ "$ENABLE_BOT_COMMAND" == "True" ]; then
-    if [ "$run_cmd" == 'jdbot' ]; then
-      # 启动jdbot安装依赖等操作操作放到后台，不耽阻塞定crontab启动工作
-      echo "后台启动jdbot程序..."
-      sh "$BOT_DIR/jdbot.sh" >>"$LOGS_DIR/jdbot_start.log" 2>&1 &
-    fi
-    echo "启动crontab定时任务主进程..."
-    crond -f
-  else
-    echo "启动crontab定时任务主进程..."
-    crond -f
+
+  if [ "$run_cmd" == 'jdbot' ]; then
+    # 启动jdbot安装依赖等操作操作放到后台，不耽阻塞定crontab启动工作
+    echo "后台启动jdbot程序..."
+    sh "$BOT_DIR/jdbot.sh" >>"$LOGS_DIR/jdbot_start.log" 2>&1 &
   fi
+  echo "启动crontab定时任务主进程..."
+  crond -f
 else
   echo "默认定时任务执行结束。"
 fi
