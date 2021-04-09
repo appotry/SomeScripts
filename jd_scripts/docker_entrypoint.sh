1fe539f8cf867a1e46833fddbc6ccacf3f69a96a
FKPYW <FKPYW>
2021-04-10 02:02:15 +0800

    new way jd bot test

--- jd_scripts/docker_entrypoint.sh
+++ jd_scripts/docker_entrypoint.sh
@@ -1,22 +1,44 @@
 #!/bin/sh
 set -e
 
-#获取配置的自定义参数
-if [ -n "$1" ]; then
-    run_cmd=$1
+function initNodeEnv() {
+  echo "安装执行脚本需要的nodejs环境及依赖"
+  apk add --update nodejs moreutils npm curl jq
+}
+
+#获取配置的自定义参数,如果有为
+if [ "$1" ]; then
+  initNodeEnv
+  run_cmd=$1
+fi
+[ -f /scripts/package.json ] && before_package_json=$(cat /scripts/package.json)
+
+if [ -f "/scripts/logs/pull.lock" ]; then
+  echo "存在更新锁定文件，跳过git pull操作..."
+else
+  echo "设定远程仓库地址..."
+  cd /scripts
+  git remote set-url origin "$REPO_URL"
+  git reset --hard
+  echo "git pull拉取最新代码..."
+  git -C /scripts pull --rebase
+  if [ ! -d /scripts/node_modules ]; then
+    echo "容器首次启动，执行npm install..."
+    npm install --loglevel error --prefix /scripts
+  else
+    if [[ "${before_package_json}" != "$(cat /scripts/package.json)" ]]; then
+      echo "package.json有更新，执行npm install..."
+      npm install --loglevel error --prefix /scripts
+    else
+      echo "package.json无变化，跳过npm install..."
+    fi
+  fi
 fi
 
-echo "设定远程仓库地址..."
-cd /scripts
-git remote set-url origin $REPO_URL
-echo "git pull拉取最新代码..."
-git -C /scripts reset --hard
-git -C /scripts pull origin master --rebase
-echo "npm install 安装最新依赖..."
-npm install --loglevel error --prefix /scripts
-echo "更新jds仓库文件..."
-git -C /jds reset --hard
-git -C /jds pull origin master --rebase
+#任务脚本shell仓库
+cd /jds
+git pull origin master --rebase
+
 #默认启动telegram交互机器人的条件
 #确认容器启动时调用的docker_entrypoint.sh
 #必须配置TG_BOT_TOKEN、TG_USER_ID，
@@ -29,22 +51,22 @@ else
 fi
 
 echo "------------------------------------------------执行定时任务任务shell脚本------------------------------------------------"
-sh -x /jds/jd_scripts/default_task.sh $ENABLE_BOT_COMMAND $run_cmd
+sh /jds/jd_scripts/shell_default_script.sh "$ENABLE_BOT_COMMAND" "$run_cmd"
 echo "--------------------------------------------------默认定时任务执行完成---------------------------------------------------"
 
-if [ -n "$run_cmd" ]; then
-    #增加一层jd_bot指令已经正确安装成功校验
-    #以上条件都满足后会启动jd_bot交互，否则还是按照以前的模式启动，最大程度避免现有用户改动调整
-    if [[ "$ENABLE_BOT_COMMAND" == "True" && -f /usr/bin/jd_bot ]]; then
-        echo "启动crontab定时任务主进程..."
-        crond
-        echo "启动telegram bot指令交主进程..."
-        jd_bot
-    else
-        echo "启动crontab定时任务主进程..."
-        crond -f
+if [ "$run_cmd" ]; then
+  if [ "$ENABLE_BOT_COMMAND" == "True" ]; then
+    if [ "$run_cmd" == 'jdbot' ]; then
+      # 启动jdbot安装依赖等操作操作放到后台，不耽阻塞定crontab启动工作
+      echo "后台启动jdbot程序..."
+      sh "$BOT_DIR/jdbot.sh" >>"$LOGS_DIR/jdbot_start.log" 2>&1 &
     fi
+    echo "启动crontab定时任务主进程..."
+    crond -f
+  else
+    echo "启动crontab定时任务主进程..."
+    crond -f
+  fi
 else
-    echo "默认定时任务执行结束。"
+  echo "默认定时任务执行结束。"
 fi
-echo -e "\n\n"
--- jd_scripts/shell_mod_script.sh
+++ jd_scripts/shell_mod_script.sh
@@ -1,30 +1,40 @@
 #!/bin/sh
+set -e
 
-if [[ -f /usr/bin/jd_bot && -z "$DISABLE_SPNODE" ]]; then
-   CMD="spnode"
+mergedListFile="/scripts/docker/merged_list_file.sh"
+remoteListFile="/jds/jd_scripts/remote_crontab_list.sh"
+
+echo "附加功能1，使用jds仓库的gen_code_conf.list文件"
+cp /jds/jd_scripts/gen_code_conf.list "$GEN_CODE_LIST"
+
+echo "附加功能2，拉取monk-coder仓库的代码，并增加相关任务"
+if [ ! -d "/monk/" ]; then
+    echo "未检查到monk-coder仓库脚本，初始化下载相关脚本..."
+    git clone https://github.com/Aaron-lv/monk-coder_backup /monk
 else
-   CMD="node"
+    echo "更新monk-coder脚本相关文件..."
+    git -C /monk reset --hard
+    git -C /monk pull origin dust --rebase
 fi
 
-echo "处理jd_crazy_joy_coin任务..."
-if [ ! $CRZAY_JOY_COIN_ENABLE ]; then
-   echo "默认启用jd_crazy_joy_coin,杀掉jd_crazy_joy_coin任务，并重启"
-   eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
-   echo '' >/scripts/logs/jd_crazy_joy_coin.log
-   $CMD /scripts/jd_crazy_joy_coin.js |ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
-   echo "默认jd_crazy_joy_coin,重启完成"
-else
-   if [ $CRZAY_JOY_COIN_ENABLE = "Y" ]; then
-      echo "配置启用jd_crazy_joy_coin,杀掉jd_crazy_joy_coin任务，并重启"
-      eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
-      echo '' >/scripts/logs/jd_crazy_joy_coin.log
-      $CMD /scripts/jd_crazy_joy_coin.js |ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
-      echo "配置jd_crazy_joy_coin,重启完成"
-   else
-      eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
-      echo "已配置不启用jd_crazy_joy_coin任务,不处理"
-   fi
+if [ -n "$(ls /monk/car/*_*.js)" ]; then
+  cp -f /monk/car/*_*.js /scripts
 fi
+if [ -n "$(ls /monk/i-chenzhe/*_*.js)" ]; then
+  cp -f /monk/i-chenzhe/*_*.js /scripts
+fi
+if [ -n "$(ls /monk/member/*_*.js)" ]; then
+  cp -f /monk/member/*_*.js /scripts
+fi
+if [ -n "$(ls /monk/normal/*_*.js)" ]; then
+  cp -f /monk/normal/*_*.js /scripts
+fi
+sed -i "/^$/d" $remoteListFile
+cat $remoteListFile >> $mergedListFile
+
+echo "替换node使用spnode执行任务"
+sed -i "s/node/spnode/g" "$mergedListFile"
+sed -i "/\(jd_carnivalcity.js\|jd_car_exchange.js\)/s/spnode/spnode conc/g" "$mergedListFile"
 
 # 修改十元街定时
 sed -i "s/12 8,18 \* \* \* node \/scripts\/jd_syj.js/2 8,18 \* \* \* node \/scripts\/jd_syj.js/g" /scripts/docker/merged_list_file.sh
