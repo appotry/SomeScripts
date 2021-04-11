6ce48b38d541936df057b1cade2e180ceee08dfd
ooo <ooo@iMac.local>
2021-04-12 01:37:46 +0800

    update updateTeam

--- updateTeam/crontab_list.sh
+++ updateTeam/crontab_list.sh
@@ -1,2 +1,13 @@
 # 每3天的23:50分清理一次日志
 50 23 */3 * * rm -rf /logs/*.log
+
+# 京喜工厂自动开团
+0 * * * * node /runscripts/jd_jxFactoryCreateTuan.js >> /logs/jd_jxFactoryCreateTuan.log 2>&1
+# 抢京豆邀请码
+0 * * * * node /runscripts/jd_updateBeanHome.js >> /logs/jd_updateBeanHome.log 2>&1
+# 领现金
+0 * * * * node /runscripts/jd_updateCash.js >> /logs/jd_updateCash.log 2>&1
+# 东东小窝邀请码
+0 * * * * node /runscripts/jd_update_home.js >> /logs/jd_update_home.log 2>&1
+# 赚京豆小程序
+0 * * * * node /runscripts/jd_zzUpdate.js >> /logs/jd_zzUpdate.log 2>&1
--- updateTeam/proc_file.sh
+++ updateTeam/proc_file.sh
@@ -30,17 +30,6 @@ else
     fi
 fi
 
-echo "京东京喜工厂自动开团..."
-node /runscripts/jd_jxFactoryCreateTuan.js >> /logs/jd_jxFactoryCreateTuan.log 2>&1
-echo "更新抢京豆邀请码..."
-node /runscripts/jd_updateBeanHome.js >> /logs/jd_updateBeanHome.log 2>&1
-echo "更新领现金..."
-node /runscripts/jd_updateCash.js >> /logs/jd_updateCash.log 2>&1
-echo "更新东东小窝邀请码..."
-node /runscripts/jd_update_home.js >> /logs/jd_update_home.log 2>&1
-echo "赚京豆小程序..."
-node /runscripts/jd_zzUpdate.js >> /logs/jd_zzUpdate.log 2>&1
-
 echo "更新updateTeam仓库文件..."
 cp -rf /runscripts/shareCodes /updateTeam/
 echo "提交updateTeam仓库文件..."
