1e5d41cca6e88d21b10c4f97703a947c92c3fd13
ooo <ooo@iMac.local>
2021-04-12 01:47:15 +0800

    update

--- updateTeam/crontab_list.sh
+++ updateTeam/crontab_list.sh
@@ -2,12 +2,12 @@
 50 23 */3 * * rm -rf /logs/*.log
 
 # 京喜工厂自动开团
-0 * * * * node /runscripts/jd_jxFactoryCreateTuan.js >> /logs/jd_jxFactoryCreateTuan.log 2>&1
+0 * * * * cd /runscripts && node jd_jxFactoryCreateTuan.js >> /logs/jd_jxFactoryCreateTuan.log 2>&1
 # 抢京豆邀请码
-0 * * * * node /runscripts/jd_updateBeanHome.js >> /logs/jd_updateBeanHome.log 2>&1
+0 * * * * cd /runscripts && node jd_updateBeanHome.js >> /logs/jd_updateBeanHome.log 2>&1
 # 领现金
-0 * * * * node /runscripts/jd_updateCash.js >> /logs/jd_updateCash.log 2>&1
+0 * * * * cd /runscripts && node jd_updateCash.js >> /logs/jd_updateCash.log 2>&1
 # 东东小窝邀请码
-0 * * * * node /runscripts/jd_update_home.js >> /logs/jd_update_home.log 2>&1
+0 * * * * cd /runscripts && node jd_update_home.js >> /logs/jd_update_home.log 2>&1
 # 赚京豆小程序
-0 * * * * node /runscripts/jd_zzUpdate.js >> /logs/jd_zzUpdate.log 2>&1
+0 * * * * cd /runscripts && node jd_zzUpdate.js >> /logs/jd_zzUpdate.log 2>&1
